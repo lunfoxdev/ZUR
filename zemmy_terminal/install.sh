d0f550390acc8e7c7468c822bf367f17dc3fe3f3
lunfoxdev <test.test6767ss@gmail.com>
2025-12-17 23:10:47 +0100

    Initial commit: dodanie paczek obsstudio i zemmy_terminal

---
+++ zemmy_terminal/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Instalacja Zemmy Terminal..."
+# Tutaj dodaj komendy instalacyjne
---
+++ zemmy_terminal/uninstall.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Odinstalowanie Zemmy Terminal..."
+# Tutaj dodaj komendy do odinstalowania
